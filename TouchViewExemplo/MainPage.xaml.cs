37fc6718481f4e03be34370d5b23564281f59aa2
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2019-06-27 11:04:11 -0300

    :tada: Liberado Exemplo no Github

---
+++ TouchViewExemplo/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TouchViewExemplo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public void Handle_AnimationStarted(object sender, EventArgs e)
+        {
+     
+        }
+
+        public void Handle_Completed(object sender, EventArgs e)
+        {
+            //DisplayAlert("Aeeeee", "Voce Clicou aqui", "OK"); 
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
